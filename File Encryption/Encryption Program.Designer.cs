8958e69c895068cbbbb1353b6e1db6597ce48ccc
IvanPotgieter01 <109952133+IvanPotgieter01@users.noreply.github.com>
2022-10-21 18:42:29 +0200

    Add project files.

---
+++ File Encryption/Encryption Program.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace File_Encryption
+{
+    partial class Encryption_Program
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Encryption_Program));
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.lblDecrypt = new System.Windows.Forms.Label();
+            this.lblEncrypt = new System.Windows.Forms.Label();
+            this.tbpasswordDecrypt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrowseDecrypt = new System.Windows.Forms.Button();
+            this.tbFilePathDecrypt = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecrypt.Location = new System.Drawing.Point(429, 110);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(372, 51);
+            this.btnDecrypt.TabIndex = 44;
+            this.btnDecrypt.Text = "Save decrypted file (encrypted file will be deleted)";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // lblDecrypt
+            // 
+            this.lblDecrypt.AutoSize = true;
+            this.lblDecrypt.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lblDecrypt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDecrypt.Cursor = System.Windows.Forms.Cursors.No;
+            this.lblDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDecrypt.Location = new System.Drawing.Point(476, 22);
+            this.lblDecrypt.Name = "lblDecrypt";
+            this.lblDecrypt.Size = new System.Drawing.Size(160, 19);
+            this.lblDecrypt.TabIndex = 50;
+            this.lblDecrypt.Text = "Select file to DECRYPT:";
+            // 
+            // lblEncrypt
+            // 
+            this.lblEncrypt.AutoSize = true;
+            this.lblEncrypt.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lblEncrypt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblEncrypt.Cursor = System.Windows.Forms.Cursors.No;
+            this.lblEncrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEncrypt.Location = new System.Drawing.Point(59, 19);
+            this.lblEncrypt.Name = "lblEncrypt";
+            this.lblEncrypt.Size = new System.Drawing.Size(160, 19);
+            this.lblEncrypt.TabIndex = 49;
+            this.lblEncrypt.Text = "Select file to ENCRYPT:";
+            // 
+            // tbpasswordDecrypt
+            // 
+            this.tbpasswordDecrypt.Location = new System.Drawing.Point(562, 64);
+            this.tbpasswordDecrypt.Name = "tbpasswordDecrypt";
+            this.tbpasswordDecrypt.PasswordChar = '*';
+            this.tbpasswordDecrypt.Size = new System.Drawing.Size(207, 20);
+            this.tbpasswordDecrypt.TabIndex = 48;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label1.Cursor = System.Windows.Forms.Cursors.No;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(456, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 47;
+            this.label1.Text = "Password:";
+            // 
+            // btnBrowseDecrypt
+            // 
+            this.btnBrowseDecrypt.BackColor = System.Drawing.SystemColors.Window;
+            this.btnBrowseDecrypt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBrowseDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseDecrypt.Location = new System.Drawing.Point(669, 19);
+            this.btnBrowseDecrypt.Name = "btnBrowseDecrypt";
+            this.btnBrowseDecrypt.Size = new System.Drawing.Size(73, 25);
+            this.btnBrowseDecrypt.TabIndex = 46;
+            this.btnBrowseDecrypt.Text = "Browse";
+            this.btnBrowseDecrypt.UseVisualStyleBackColor = false;
+            this.btnBrowseDecrypt.Click += new System.EventHandler(this.btnBrowseDecrypt_Click);
+            // 
+            // tbFilePathDecrypt
+            // 
+            this.tbFilePathDecrypt.Location = new System.Drawing.Point(462, 126);
+            this.tbFilePathDecrypt.Name = "tbFilePathDecrypt";
+            this.tbFilePathDecrypt.ReadOnly = true;
+            this.tbFilePathDecrypt.Size = new System.Drawing.Size(291, 20);
+            this.tbFilePathDecrypt.TabIndex = 45;
+            this.tbFilePathDecrypt.Visible = false;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEncrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncrypt.Location = new System.Drawing.Point(12, 110);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(372, 51);
+            this.btnEncrypt.TabIndex = 43;
+            this.btnEncrypt.Text = "Save encrypted file (origional file will be deleted)";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(145, 65);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(207, 20);
+            this.tbPassword.TabIndex = 42;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lblPassword.Cursor = System.Windows.Forms.Cursors.No;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(39, 68);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(73, 17);
+            this.lblPassword.TabIndex = 41;
+            this.lblPassword.Text = "Password:";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.SystemColors.Window;
+            this.btnBrowse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(243, 14);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(73, 26);
+            this.btnBrowse.TabIndex = 40;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(25, 126);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.ReadOnly = true;
+            this.tbPath.Size = new System.Drawing.Size(291, 20);
+            this.tbPath.TabIndex = 39;
+            this.tbPath.Visible = false;
+            // 
+            // Encryption_Program
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::File_Encryption.Properties.Resources.Backgorund;
+            this.ClientSize = new System.Drawing.Size(817, 180);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.lblDecrypt);
+            this.Controls.Add(this.lblEncrypt);
+            this.Controls.Add(this.tbpasswordDecrypt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrowseDecrypt);
+            this.Controls.Add(this.tbFilePathDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.tbPath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Encryption_Program";
+            this.Text = "Encryption_Program";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label lblDecrypt;
+        private System.Windows.Forms.Label lblEncrypt;
+        private System.Windows.Forms.TextBox tbpasswordDecrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrowseDecrypt;
+        private System.Windows.Forms.TextBox tbFilePathDecrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox tbPath;
+    }
+}
---
+++ File Encryption/Encryption Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace File_Encryption
+{
+    public partial class Encryption_Program : Form
+    {
+        public Encryption_Program()
+        {
+            InitializeComponent();
+        }
+
+        string fileOrigional = "";  //Declaring global variables
+        string fileEncrypted = "";
+
+        public static byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null; //Declaring empty array of salt bytes
+
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 }; //Assigning values to the array
+
+            using (MemoryStream memorystrm = new MemoryStream()) //Encapsulating data to be accessed by memory
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256; //Setting key size
+                    AES.BlockSize = 128; //Setting block size
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000); //Implementing password-based key derivation functionality
+                    AES.Key = key.GetBytes(AES.KeySize / 8); //Gets the size of the secret key
+                    AES.IV = key.GetBytes(AES.BlockSize / 8); //Gets initialization vector from the block size
+
+                    AES.Mode = CipherMode.CBC; //Setting block cipher mode to Cipher Block Chaining (CBC)
+
+                    using (var cs = new CryptoStream(memorystrm, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length); //Writes bytes to CryptoStream 
+                        cs.Close();
+                    }
+                    encryptedBytes = memorystrm.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        public static byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null; //Declaring empty array of salt bytes
+
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };  //Assigning values to the array
+
+            using (MemoryStream memorystrm = new MemoryStream()) //Encapsulating data to be accessed by memory
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256; //Setting key size
+                    AES.BlockSize = 128; //Setting block size
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8); //Gets the size of the secret key
+                    AES.IV = key.GetBytes(AES.BlockSize / 8); //Gets initialization vector
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(memorystrm, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length); //Writes bytes to CryptoStream 
+                        cs.Close();
+                    }
+                    decryptedBytes = memorystrm.ToArray();
+                }
+            }
+
+            return decryptedBytes;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Multiselect = false; //Disallows multiple file selections
+            if (od.ShowDialog() == DialogResult.OK)
+            {
+                tbPath.Text = od.FileName; //Obtaining filepath for hidden textbox
+                fileOrigional = od.FileName; //File to be encrypt set as file selected
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            Boolean doNotEncrypt = false;  //Variable to ensure that file is ready to be encrypted (file selected + password filled in)
+
+            try
+            {
+                if (!File.Exists(tbPath.Text)) //Input validation
+                {
+                    MessageBox.Show("File does not exist."); //Error if no fileis selected or the file does not exist
+                    doNotEncrypt = true;
+                    return;
+                }
+                if (String.IsNullOrEmpty(tbPassword.Text))
+                {
+                    MessageBox.Show("Password empty. Please enter your password"); //Error if no password is filled in
+                    doNotEncrypt = true;
+                    return;
+                }
+                if (!doNotEncrypt) //Case if file is ready to be encrypted
+                {
+                    EncryptFile();
+                    File.Delete(tbPath.Text); //Deleting origional file after encrypted file has been saved
+                    MessageBox.Show("Encryption successful!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error!");
+            }
+        }
+
+        private void btnBrowseDecrypt_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Multiselect = false;
+            if (od.ShowDialog() == DialogResult.OK) //File to be decrypt set as file selected
+            {
+                fileEncrypted = od.FileName;
+                tbFilePathDecrypt.Text = od.FileName;  //Obtaining filepath for hidden textbox
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            Boolean noFileSelected = true; //Variable to ensure that file is ready to be decrypted (file selected + password filled in)
+
+            if (tbFilePathDecrypt.Text != "")
+            {
+                noFileSelected = false; //Case is file path is selected
+            }
+
+            try
+            {
+                DecryptFile();
+                File.Delete(tbFilePathDecrypt.Text); //Deleting ecrypted file after decrypted file has been saved
+                MessageBox.Show("Decryption successful!");
+            }
+            catch (Exception ex)
+            {
+                if (noFileSelected)
+                {
+                    MessageBox.Show("No file selected"); //Error if no file was selected
+                }
+                else
+                {
+                    MessageBox.Show("Password incorrect"); //Error if password is incorrect
+                }
+            }
+        }
+
+        public void EncryptFile()
+        {
+            string password = ""; //Declaring password as empty string
+
+            password = tbPassword.Text; //Setting password to the user provided password
+
+            byte[] bytesToBeEncrypted = File.ReadAllBytes(fileOrigional); //Reads the contents of the file into the byte array
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password); //Encoding characters using password
+
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes); //Hashing the password with SHA256
+
+            byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes); //Calling AES_Encrypt method
+
+            String fileExt = Path.GetExtension(tbPath.Text); //Obtaining file extention from file path
+            SaveFileDialog sd = new SaveFileDialog(); //Showing Save File Diaglog box to save te encrypted file
+            sd.Filter = "Files (*" + fileExt + ") | *" + fileExt; //Adding file extention of the origional file
+            if (sd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllBytes(sd.FileName, bytesEncrypted); //Saving encrypted file to selected location
+            }
+        }
+
+        public void DecryptFile()
+        {
+            string password = "";
+
+            password = tbpasswordDecrypt.Text;
+
+            byte[] bytesToBeDecrypted = File.ReadAllBytes(fileEncrypted); //Reads the contents of the file into the byte array
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password); //Encoding characters using password
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesDecrypted = AES_Decrypt(bytesToBeDecrypted, passwordBytes); //Calling AES_Decrypt method
+
+            String fileExt = Path.GetExtension(tbFilePathDecrypt.Text);
+            SaveFileDialog sd = new SaveFileDialog();
+            sd.Filter = "Files (*" + fileExt + ") | *" + fileExt;
+            if (sd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllBytes(sd.FileName, bytesDecrypted); //Saving decrypted file to selected location
+            }
+        }
+        
+    }
+}
